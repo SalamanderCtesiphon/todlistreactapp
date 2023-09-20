9756ca3af1402b9643df1c75e323b3775cda5da4
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-20 13:04:27 -0500

    updating the dox

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [text, setText] = useState('')
 
   return (
     <>
     <header>
       <h1>To Do List</h1>
     </header>
+    <main>
+      <button onClick={() => console.log('hi')}>add to do</button>
+    </main>
     </>
   )
 }
