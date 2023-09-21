7d22152313851ac6360bb72fe9e0e025e5df23c4
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-21 14:26:28 -0500

    created a text input for a to do list

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 function App() {
   const [counter, setCounter] = useState(0)
 
+  const addTodo = () => {
+    console.log("Howdy!")
+  }
+
   useEffect(() => {
     const key = setInterval(() => {
       setCounter(count => count + 1)
@@ -17,6 +21,9 @@ function App() {
   return (
     <>
       <p>{counter} seconds have passed</p>
+      <label htmlFor="todo">Enter a to do:</label>
+      <input type="text" name="todo" id="todo" />
+      <button onClick={addTodo}>Add To Do</button>
     </>
   )
 }
