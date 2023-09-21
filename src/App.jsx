372c7534fff94c5ea12dd7e15a4d85ac490254ae
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-21 14:30:44 -0500

    implemneted the state variables and functions to create an array of todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [todo, setTodo] = useState('test to do')
+  const [todos, setTodos] = useState([])
 
   const addTodo = () => {
-    console.log("Howdy!")
+    setTodos([...todos, todo])
   }
 
   useEffect(() => {
@@ -18,11 +20,21 @@ function App() {
     }
   }, [])
 
+  useEffect(() => {
+    console.log(todos)
+  }, [todos])
+
   return (
     <>
       <p>{counter} seconds have passed</p>
       <label htmlFor="todo">Enter a to do:</label>
-      <input type="text" name="todo" id="todo" />
+      <input 
+        type="text" 
+        name="todo" 
+        id="todo"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)} 
+      />
       <button onClick={addTodo}>Add To Do</button>
     </>
   )
